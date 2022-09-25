e367265510e9c0a7862e4c63367ae53ac0b6aea5
Pierre-Emmanuel Denys <pedenys@lamobilery.fr>
2022-09-25 16:33:06 +0200

    feat(colorSwitch): add last used color preference

--- src/scripts/colorPicker.ts
+++ src/scripts/colorPicker.ts
@@ -1,23 +1,56 @@
 const arrColors: string[] = ["#C0ECCC", "#F9F0C1", "#A5C8E4", "#F6A8A6"]; // green yellow blue red
 const hoverColors: string[] = ["#007f00", "#877800", "#00007F", "#610d0a"]; // green yellow blue red
+const fillColors: string[] = ["#C0ECCC", "#F9F0C1", "#A5C8E4", "#F6A8A6"]; // green yellow blue red
+
+const CURRENT_COLOR_NUMBER_LOCAL_STORAGE_ITEM = "currentColorNumber";
+
 let currentColor = 0;
 let nextColor = 1;
 
-function updateDOMcolors() {
-  if (typeof window !== "undefined" && document) {
+function setStoredColor() {
+  const storedColor = localStorage.getItem(
+    CURRENT_COLOR_NUMBER_LOCAL_STORAGE_ITEM
+  );
+
+  if (typeof storedColor === "string") {
+    // 1. set currentColor as storedColor
+    // 2. update nextColor as storedColor + 1
+    updateCurrentAndNextColor(Number(storedColor));
+    // 3. paint DOM
     const backgroundColor = arrColors[currentColor] as string;
-    const nextBackgroundColor = hoverColors[nextColor] as string;
+    const nextBackgroundColor = fillColors[nextColor] as string;
     const hoverColor = hoverColors[currentColor] as string;
 
-    // console.log("🍕", {
-    //   currentColor,
-    //   nextColor,
-    //   backgroundColor,
-    //   nextBackgroundColor,
-    //   hoverColor,
-    // });
+    updateDOMcolors({ backgroundColor, nextBackgroundColor, hoverColor });
+  }
+}
 
-    document.body.style.backgroundColor = backgroundColor;
+function storeColor(currentColorNumber: number) {
+  localStorage.setItem(
+    CURRENT_COLOR_NUMBER_LOCAL_STORAGE_ITEM,
+    currentColorNumber.toString()
+  );
+}
+
+function updateDOMcolors({
+  backgroundColor,
+  nextBackgroundColor,
+  hoverColor,
+}: {
+  backgroundColor: string;
+  nextBackgroundColor: string;
+  hoverColor: string;
+}) {
+  if (typeof window !== "undefined" && document) {
+    console.log("🍕", {
+      currentColor,
+      nextColor,
+      backgroundColor,
+      nextBackgroundColor,
+      hoverColor,
+    });
+
+    document.body.style.setProperty("--tintColor", backgroundColor);
     document.body.style.setProperty("--hoverColor", hoverColor);
     (document.getElementById("circle") as HTMLElement).style.fill =
       nextBackgroundColor;
@@ -38,32 +71,51 @@ function updateDOMcolors() {
       // red
       document.body.style.setProperty("--filterEffect", "hue-rotate(220deg)");
     }
+    storeColor(currentColor);
+  }
+}
+function updateCurrentAndNextColor(newColorNumber?: number) {
+  if (typeof newColorNumber === "number") {
+    currentColor = newColorNumber;
+    nextColor = newColorNumber + 1;
+    if (nextColor > 4) {
+      nextColor = 0;
+    }
+    return;
+  }
+
+  currentColor = nextColor;
+  nextColor = nextColor + 1;
+  if (nextColor > 4) {
+    nextColor = 0;
   }
 }
 
 function handleClickOnColor() {
-  currentColor = nextColor;
-  nextColor = nextColor + 1;
-  if (nextColor === 4) {
-    nextColor = 0;
-  }
-  updateDOMcolors();
+  updateCurrentAndNextColor();
+
+  const backgroundColor = arrColors[currentColor] as string;
+  const nextBackgroundColor = fillColors[nextColor] as string;
+  const hoverColor = hoverColors[currentColor] as string;
+
+  updateDOMcolors({ backgroundColor, nextBackgroundColor, hoverColor });
 }
 
 function addListener() {
   if (typeof window !== "undefined") {
+    setStoredColor();
     document
       .querySelector("#circle-container")
       ?.addEventListener("click", function () {
         handleClickOnColor();
       });
-    // updateDOMcolors();
-    clearInterval(addListenerInterval);
+
+    // clearInterval(addListenerInterval);
   }
 }
 
-const addListenerInterval = setInterval(() => {
-  addListener();
-}, 100);
+// const addListenerInterval = setInterval(() => {
+addListener();
+// }, 100);
 
 export {};
