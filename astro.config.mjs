ca6ca38fe7ee326fce4e8279ffe9479569c36609
pedenys <pierreemmanuel.denys@gmail.com>
2024-09-16 11:03:58 +0200

    feat: add i18n config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,16 @@
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  i18n: {
+    defaultLocale: "en",
+    locales: ["en", "fr"],
+    routing: {
+      prefixDefaultLocale: false,
+      fallback: "rewrite",
+    },
+    fallback: {
+      fr: "en",
+    },
+  },
+});
