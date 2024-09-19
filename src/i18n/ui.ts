01cfad3f4cbf906fbb43a88bf34627946837bc9f
pedenys <pierreemmanuel.denys@gmail.com>
2024-09-19 12:59:28 +0200

    feat: add ui strings and align footer elements

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -7,9 +7,19 @@ export const defaultLang = "en";
 
 export const ui = {
   en: {
+    "common.and": "and",
     "menu.entries": "Entries",
+    "intro.minimalist": "Minimalist",
+    "intro.developerTraindIn": " software developer, trained in",
+    "intro.continentalPhilosophy": "continental philosophy",
+    "footer.absent": `I'm also not here on`,
   },
   fr: {
+    "common.and": "et",
     "menu.entries": "Entrées",
+    "intro.minimalist": "Minimaliste",
+    "intro.developerTraindIn": ", développeur formé à la",
+    "intro.continentalPhilosophy": "philosophie",
+    "footer.absent": `Je suis également absent sur`,
   },
 } as const;
