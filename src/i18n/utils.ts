6645200823c375ea34f9e9bffaa2a1aab5fa88ad
pedenys <pierreemmanuel.denys@gmail.com>
2024-11-09 07:53:40 +0100

    feat: remove english language

--- src/i18n/utils.ts
+++ src/i18n/utils.ts
@@ -1,46 +1,10 @@
 import { ui, defaultLang } from "./ui";
 
-export function getLangFromUrl(url: URL) {
-  const [, lang] = url.pathname.split("/");
-  if (lang && lang in ui) return lang as keyof typeof ui;
-  return defaultLang;
-}
 
-export function useTranslations(lang: keyof typeof ui) {
+export function useTranslations() {
   return function t(key: keyof (typeof ui)[typeof defaultLang]) {
-    return ui[lang][key] || ui[defaultLang][key];
+    return ui["fr"][key] || ui[defaultLang][key];
   };
 }
 
-export function getUrl({
-  language,
-  origin,
-  pathname,
-}: {
-  language: string;
-  origin: string;
-  pathname: string;
-}) {
-  if (language === "fr") {
-    if (pathname.includes("/fr/")) {
-      return `${origin}${pathname.replace("/fr/", "/en/")}`;
-    }
 
-    if (pathname === "/fr" || !pathname) {
-      return `${origin}`;
-    }
-
-    return `${origin}${pathname}`;
-  }
-
-  if (language === "en") {
-    if (pathname.includes("/en/")) {
-      return `${origin}${pathname.replace("/en/", "/fr/")}`;
-    }
-
-    if (pathname === "/" || !pathname) {
-      return `${origin}/fr`;
-    }
-    return `${origin}/fr${pathname}`;
-  }
-}
